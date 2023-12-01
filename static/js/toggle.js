2bb6c64865a7af1ebd034e89392e509d53a36f7e
CesarArathAP <1722110585@utectulancingo.edu.mx>
2023-11-30 18:49:33 -0600

    UPDATE toggle and index

--- static/js/toggle.js
+++ static/js/toggle.js
@@ -5,6 +5,37 @@ document.addEventListener('DOMContentLoaded', function () {
     ledToggle.addEventListener('change', function () {
         const isLedOn = ledToggle.checked;
         ledState.textContent = isLedOn ? 'Encendido (ON)' : 'Apagado (OFF)';
-        // Agrega aquí la lógica para encender o apagar el LED según el estado
+        
+        // Actualiza el valor del LED en el servidor
+        updateLedState(isLedOn);
     });
+
+    // Función para realizar la solicitud al servidor
+    function updateLedState(isLedOn) {
+        const url = 'https://backend-iot-9945b3a20353.herokuapp.com/dispositivos/1';
+        const payload = {
+            valor: isLedOn ? '1' : '0'
+        };
+
+        fetch(url, {
+            method: 'PUT',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(payload)
+        })
+        .then(response => {
+            if (!response.ok) {
+                throw new Error('Error al actualizar el estado del LED');
+            }
+            return response.json();
+        })
+        .then(data => {
+            // Maneja la respuesta del servidor si es necesario
+            console.log('Respuesta del servidor:', data);
+        })
+        .catch(error => {
+            console.error('Error:', error);
+        });
+    }
 });
